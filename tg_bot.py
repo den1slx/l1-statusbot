7d6f849ed5f7999a82a73910b2b4f7142b08161b
den1slx <keycoingold@mail.ru>
2023-07-23 16:45:14 +1000

    Delete logging

--- tg_bot.py
+++ tg_bot.py
@@ -2,7 +2,6 @@ import requests
 from requests.exceptions import ReadTimeout, ConnectionError as RequestsConnectionError
 
 import json
-import logging
 from time import sleep
 
 from tg_bot_env import bot, devman_token, available_chat_ids, telebot
@@ -32,27 +31,18 @@ def check_status(chat_id, devman_token):
     }
     params = ''
     while True:
-        logging.info('start')
         try:
-            logging.info('begin try')
             lp_response = requests.get(long_polling_url, headers=headers, params=params, timeout=120)
-            logging.info(lp_response)
         except ReadTimeout:
-            logging.warning('ReadTimeout')
             continue
         except RequestsConnectionError:
-            logging.warning('requests.exceptions.ConnectionError')
             sleep(5)
             continue
-        logging.info('end try - except')
         text = json.loads(lp_response.text)
-        logging.info(f'json.loads: {text}')
         if text['status'] == 'found':
             for notification in create_notifications(text):
                 bot.send_message(chat_id, notification)
         elif text['status'] == 'timeout':
-            if not text['request_query'] and params:
-                logging.warning(f'not request_query: {text}')
             params = {'timestamp': f"{text['timestamp_to_request']}"}
         else:
             params = ''
@@ -71,8 +61,6 @@ def start_bot():
 
 
 def main():
-    logging.basicConfig(level=logging.INFO, filename="bot_log.log", filemode="w")
-
     start_bot()
 
 
